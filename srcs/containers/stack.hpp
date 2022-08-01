07bd338b7e310cbcb6516cc2a79fd523359c3e84
Sophie <socarboni@gmail.com>
2022-08-01 15:35:47 +0200

    cleaning stack useless comments

--- srcs/containers/stack.hpp
+++ srcs/containers/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 15:15:42 by scarboni          #+#    #+#             */
-/*   Updated: 2022/07/29 11:59:20 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/08/01 15:35:04 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,139 +39,32 @@ namespace ft
 		_Sequence c;
 
 	public:
-		/* Constructs a stack container adaptor object.
-		 * A container adaptor keeps internally a container object as data.
-		 * This container object is a copy of the ctnr argument passed to the constructor,
-		 * if any, otherwise it is an empty container.
-		 */
-		explicit stack(const _Sequence &__c = _Sequence())
-			: c(__c)
-		{
-		}
-
-		/*
-		 *  Returns true if the stack is empty.
-		 */
-		bool empty() const
-		{
-			return c.empty();
-		}
-
-		/*  Returns the number of elements in the stack.  */
-		size_type size() const
-		{
-			return c.size();
-		}
-
-		/*
-		 *  Returns a read/write reference to the data at the first
-		 *  element of the stack.
-		 */
-		reference top()
-		{
-			return c.back();
-		}
-
-		/*
-		 *  Returns a read-only (constant) reference to the data at the first
-		 *  element of the stack.
-		 */
-		const_reference top() const
-		{
-			return c.back();
-		}
-
-		/*
-		 *  @brief  Add data to the top of the stack.
-		 *  @param  __x  Data to be added.
-		 *
-		 *  This is a typical stack operation.  The function creates an
-		 *  element at the top of the stack and assigns the given data
-		 *  to it.  The time complexity of the operation depends on the
-		 *  underlying sequence.
-		 */
-		void push(const value_type &__x)
-		{
-			c.push_back(__x);
-		}
-
-		/*
-		 *  @brief  Removes first element.
-		 *
-		 *  This is a typical stack operation.  It shrinks the stack
-		 *  by one.  The time complexity of the operation depends on the
-		 *  underlying sequence.
-		 *
-		 *  Note that no data is returned, and if the first element's
-		 *  data is needed, it should be retrieved before pop() is
-		 *  called.
-		 */
-		void pop()
-		{
-			c.pop_back();
-		}
+		explicit stack(const _Sequence &__c = _Sequence()) : c(__c) {}
+		bool empty() const { return c.empty(); }
+		size_type size() const { return c.size(); }
+		reference top() { return c.back(); }
+		const_reference top() const { return c.back(); }
+		void push(const value_type &__x) { c.push_back(__x); }
+		void pop() { c.pop_back(); }
 	};
 
-	/*
-	 *  @brief  Stack equality comparison.
-	 *  @param  __x  A stack.
-	 *  @param  __y  A stack of the same type as @a __x.
-	 *  @return  True iff the size and elements of the stacks are equal.
-	 *
-	 *  This is an equivalence relation.  Complexity and semantics
-	 *  depend on the underlying sequence type, but the expected rules
-	 *  are: this relation is linear in the size of the sequences, and
-	 *  stacks are considered equivalent if their sequences compare
-	 *  equal.
-	 */
 	template <typename _Tp, typename _Seq>
-	inline bool operator==(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y)
-	{
-		return __x.c == __y.c;
-	}
-
-	/*
-	 *  @brief  Stack ordering relation.
-	 *  @param  __x  A stack.
-	 *  @param  __y  A stack of the same type as @a x.
-	 *  @return  True iff @a x is lexicographically less than @a __y.
-	 *
-	 *  This is an total ordering relation.  Complexity and semantics
-	 *  depend on the underlying sequence type, but the expected rules
-	 *  are: this relation is linear in the size of the sequences, the
-	 *  elements must be comparable with @c <, and
-	 *  std::lexicographical_compare() is usually used to make the
-	 *  determination.
-	 */
-	template <typename _Tp, typename _Seq>
-	inline bool operator<(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y)
-	{
-		return __x.c < __y.c;
-	}
+	bool operator==(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y) { return __x.c == __y.c; }
 
 	template <typename _Tp, typename _Seq>
-	inline bool operator!=(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y)
-	{
-		return !(__x == __y);
-	}
+	bool operator<(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y) { return __x.c < __y.c; }
 
 	template <typename _Tp, typename _Seq>
-	inline bool operator>(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y)
-	{
-		return __y < __x;
-	}
+	bool operator!=(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y) { return !(__x == __y); }
 
 	template <typename _Tp, typename _Seq>
-	inline bool operator<=(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y)
-	{
-		return !(__y < __x);
-	}
+	bool operator>(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y) { return __y < __x; }
 
 	template <typename _Tp, typename _Seq>
-	inline bool operator>=(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y)
-	{
-		return !(__x < __y);
-	}
+	bool operator<=(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y) { return !(__y < __x); }
+
+	template <typename _Tp, typename _Seq>
+	bool operator>=(const stack<_Tp, _Seq> &__x, const stack<_Tp, _Seq> &__y) { return !(__x < __y); }
 }
 
 #endif
