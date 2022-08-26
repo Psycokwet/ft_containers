97605dc9b9ee0aaa5617565051a8a5e1ddad3581
Sophie <socarboni@gmail.com>
2022-08-26 11:10:01 +0200

    clean format pair

--- srcs/containers/pair.hpp
+++ srcs/containers/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 11:11:55 by scarboni          #+#    #+#             */
-/*   Updated: 2022/07/28 17:28:22 by scarboni         ###   ########.fr       */
+/*   Updated: 2022/08/25 20:32:26 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,7 @@ namespace ft
 	 *  @tparam _T2  Type of second object.
 	 */
 	template <typename _T1, typename _T2>
-	struct pair
-		: private __pair_base<_T1, _T2>
+	struct pair : private __pair_base<_T1, _T2>
 	{
 		typedef _T1 first_type;	 //	The first template parameter (T1)
 		typedef _T2 second_type; // The second template parameter (T2)
@@ -37,65 +36,38 @@ namespace ft
 		_T2 second; // The second value in the pair
 
 		/* Constructs a pair object with its elements value-initialized. */
-		pair()
-			: first(), second()
-		{
-		}
+		pair() : first(), second() {}
 
 		/* The object is initialized with the contents of the pr pair object.
 		 * The corresponding member of pr is passed to the constructor of each of its members
 		 */
 		template <typename _U1, typename _U2>
-		pair(const pair<_U1, _U2> &__p)
-			: first(__p.first), second(__p.second)
-		{
-		}
+		pair(const pair<_U1, _U2> &__p) : first(__p.first), second(__p.second) {}
 
 		/* Member first is constructed with a and member second with b. */
-		pair(const _T1 &__a, const _T2 &__b)
-			: first(__a), second(__b)
-		{
-		}
+		pair(const _T1 &__a, const _T2 &__b) : first(__a), second(__b) {}
 
 		// implicitly declared:
 		// pair& operator= (const pair& pr);
 	};
 
 	template <typename _T1, typename _T2>
-	bool operator==(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y)
-	{
-		return __x.first == __y.first && __x.second == __y.second;
-	}
+	bool operator==(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y) { return __x.first == __y.first && __x.second == __y.second; }
 
 	template <typename _T1, typename _T2>
-	bool operator<(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y)
-	{
-		return __x.first < __y.first || (!(__y.first < __x.first) && __x.second < __y.second);
-	}
+	bool operator<(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y) { return __x.first < __y.first || (!(__y.first < __x.first) && __x.second < __y.second); }
 
 	template <typename _T1, typename _T2>
-	bool operator!=(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y)
-	{
-		return !(__x == __y);
-	}
+	bool operator!=(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y) { return !(__x == __y); }
 
 	template <typename _T1, typename _T2>
-	bool operator>(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y)
-	{
-		return __y < __x;
-	}
+	bool operator>(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y) { return __y < __x; }
 
 	template <typename _T1, typename _T2>
-	bool operator<=(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y)
-	{
-		return !(__y < __x);
-	}
+	bool operator<=(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y) { return !(__y < __x); }
 
 	template <typename _T1, typename _T2>
-	bool operator>=(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y)
-	{
-		return !(__x < __y);
-	}
+	bool operator>=(const pair<_T1, _T2> &__x, const pair<_T1, _T2> &__y) { return !(__x < __y); }
 
 	/**
 	 * Constructs a pair object with its first element set to x and its second element set to y.
@@ -103,10 +75,7 @@ namespace ft
 	 * pair objects can be constructed from other pair objects containing different types, if the respective types are implicitly convertible.
 	 */
 	template <typename _T1, typename _T2>
-	pair<_T1, _T2> make_pair(_T1 __x, _T2 __y)
-	{
-		return pair<_T1, _T2>(__x, __y);
-	}
+	pair<_T1, _T2> make_pair(_T1 __x, _T2 __y) { return pair<_T1, _T2>(__x, __y); }
 
 }
 
